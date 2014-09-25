3cbd37b4bf7d980270cd08c913728622de01a0ae
takahashikzn <takahashikzn@me.com>
2014-09-25 20:31:43 +0900

    refactoring

--- master.js
+++ master.js
@@ -1,62 +1,65 @@
 // Window load event used just in case window height is dependant upon images
-var $jq = jQuery.noConflict(true);
 
-function stickyFoot() {
-  var bodyHeight = $jq("body.yui-skin-sam").height();
-  var vwptHeight = $jq(window).height();
-  var footHeight = 45;
-  var headerHeight = $jq("#header").height();
-  $jq("#main-table").css("min-height",vwptHeight-footHeight-headerHeight);
-}
+(function($) {
 
-function updateExecutors() {
-  $jq('#executors th.pane a.model-link').css('max-width',$jq("#side-panel").width() - 15);
-}
+    function stickyFoot() {
+        var bodyHeight = $("body.yui-skin-sam").height();
+        var vwptHeight = $(window).height();
+        var footHeight = 45;
+        var headerHeight = $("#header").height();
+        $("#main-table").css("min-height",vwptHeight-footHeight-headerHeight);
+    }
 
-$jq(document).ready(function() {
-  stickyFoot();
-  updateExecutors();
+    function updateExecutors() {
+        $('#executors th.pane a.model-link').css('max-width',$("#side-panel").width() - 15);
+    }
 
-  //hook into build executor update
-  _refreshPart = window.refreshPart;
-  window.refreshPart = function(id, url) {
-    _refreshPart(id, url);
-    if(id == 'executors') updateExecutors();
-  }
-});
+    $(document).ready(function() {
+        stickyFoot();
+        updateExecutors();
 
-$jq(window).resize(function() {
-  stickyFoot();
-  updateExecutors();
-});
+        // hook into build executor update
+        _refreshPart = window.refreshPart;
+        window.refreshPart = function(id, url) {
+            _refreshPart(id, url);
+            if(id == 'executors') updateExecutors();
+        }
+    });
 
-$jq(document).scroll(function() {
-  stickyFoot();
-});
+    $(window).resize(function() {
+        stickyFoot();
+        updateExecutors();
+    });
 
-$jq(document).ready(function() {
-  $jq('#main-panel')
-    .removeClass('col-md-9').addClass('col-md-8')
-    .attr('style', 'border-left: none !important;')
-      .find("div[style='float:right'] div[align='right']")
-        .attr('align', 'left');
-});
+    $(document).scroll(function() {
+        stickyFoot();
+    });
 
-setInterval(function() {
+    $(document).ready(function() {
+        $('#main-panel')
+            .removeClass('col-md-9').addClass('col-md-8')
+            .attr('style', 'border-left: none !important;')
+                .find("div[style='float:right'] div[align='right']")
+                .attr('align', 'left');
+    });
 
-    $jq(document)
-        .find("a[href^='job/'][href*='/build'], a[href='/cancelQuietDown']")
-        .each(function() {
+    setInterval(function() {
 
-            var $this = $jq(this);
+        $(document)
+            .find("a[href^='job/'][href*='/build'], a[href='/cancelQuietDown']")
+            .each(function() {
 
-            if ($this.data('forcePostInstrumented')) {
-                return;
-            }
+                var $this = $(this);
 
-            $this.data('forcePostInstrumented', true).click(function() {
-                $jq.post($this.prop('href'));
-                return false;
+                if ($this.data('forcePostInstrumented')) {
+                    return;
+                }
+
+                $this.data('forcePostInstrumented', true).click(function() {
+                    $.post($this.prop('href'));
+                    return false;
+                });
             });
-        });
-}, 100);
+    }, 100);
+
+}(jQuery.noConflict(true));
