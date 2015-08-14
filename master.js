ea384a99b7ee968e34a162f2708885e67d960b41
takahashikzn <takahashikzn@me.com>
2015-08-14 18:12:09 +0900

    サイズ調整

--- master.js
+++ master.js
@@ -114,7 +114,7 @@
                     var $video = $('<video />');
 
                     var minimumVideoControlWidth = 200;
-                    var desiredVideoWidth = Math.min(Math.floor(($jq(window).width() - $jq('#side-panel').width() - $jq("#main-panel div[style='float:right']").width()) * 0.9), 1100);
+                    var desiredVideoWidth = Math.min(Math.floor($jq(window).width() - $jq('#side-panel').width() - $jq("#main-panel div[style='float:right']").width()), 1100);
                     var videoWidthMargin = 250;
                     var videoWidth = Math.max(desiredVideoWidth - videoWidthMargin, minimumVideoControlWidth);
 
